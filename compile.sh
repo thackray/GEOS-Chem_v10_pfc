aa1d40a5784c0dc2eac71e5b3290ded46ee96bcd
thackray <thackray@mit.edu>
2016-05-02 13:32:46 -0400

    First commit

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+setenv LIBBY /home/software/intel/intel-2013_sp1.0.080/pkg/netcdf/netcdf-20130909
+setenv GC_BIN           $LIBBY/bin
+setenv GC_INCLUDE       $LIBBY/include
+setenv GC_LIB           $LIBBY/lib 
