d18776678309e35e3840ab19ef50f3a6e99f5d7c
thackray <thackray@mit.edu>
2016-05-25 11:21:07 -0400

    added g++ path needed to compile HEMCO

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@ setenv LIBBY /home/software/intel/intel-2013_sp1.0.080/pkg/netcdf/netcdf-2013090
 setenv GC_BIN           $LIBBY/bin
 setenv GC_INCLUDE       $LIBBY/include
 setenv GC_LIB           $LIBBY/lib 
+setenv GXX_ROOT /home/software/gcc/gcc-4.8.3/
